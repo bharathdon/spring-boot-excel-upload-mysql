ac2278f8f16e395cc3fa70d6c33fe78ebe07a4f5
bharath <dongisharapu999@gmail.com>
2020-11-03 15:41:45 +0530

    spring boot excel upload to mysql (some error)

--- spring-boot-excel/src/main/java/com/abc/SpringBootExcelApplication.java
+++ spring-boot-excel/src/main/java/com/abc/SpringBootExcelApplication.java
@@ -8,6 +8,7 @@ public class SpringBootExcelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExcelApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
